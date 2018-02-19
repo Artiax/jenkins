40c2811920f72f1d8e02d208b376c5f90a2c342b
Deividas Sutkus <deividas.sutkus@qa.com>
2018-02-19 16:14:26 +0000

    Use Jenkins API to retrieve list of installed plugins.

--- mappedFiles/bin/getPlugins.sh
+++ mappedFiles/bin/getPlugins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Iterate over currently installed plugins
-# Create a map of plugin:version
-# Convert the map into a JSON objects using jq
-# Wrap JSON objects with an array using jq
-find ${JENKINS_HOME}/plugins -maxdepth 1 -mindepth 1 -type d -exec cat {}/META-INF/MANIFEST.MF \; \
-  | grep -E 'Short-Name|Plugin-Version' | awk '{ print $2 }' | tr -d ' \r' | paste -d ':' - - | sort \
-  | jq -Rn '["plugin","version"] as $keys | (inputs | split(":")) as $plugins | [[$keys, $plugins] | transpose[] | {key:.[0],value:.[1]}] | from_entries' \
-  | jq -n '[inputs]'
+# Retrieve a list of plugins using the API
+# Select only the active ones
+# Output only shortName,version
+# Convert objects into array of objects
+# Sort the output by the .shortName
+curl -sg 'http://localhost:'$JENKINS_MASTER_PORT'/pluginManager/api/json?depth=1&tree=plugins[active,shortName,version]' \
+  | jq '.plugins[] | select(.active==true) | {shortName,version}' \
+  | jq '[inputs] | sort_by(.shortName)'
--- mappedFiles/bin/plugins.sh
+++ mappedFiles/bin/plugins.sh
@@ -4,15 +4,15 @@ set -e
 
 mkdir -p ${JENKINS_HOME}/plugins
 
-for plugin in $(jq '.[] | .plugin+":"+.version' $1 -r); do
+for plugin in $(jq '.[] | .shortName+":"+.version' $1 -r); do
   plugin=(${plugin//:/ })
-  name=${plugin[0]}
+  shortName=${plugin[0]}
   version=${plugin[1]}
   [[ -z ${version} ]] && version="latest"
 
-  echo "Installing plugin ${name}:${version}"
+  echo "Installing plugin ${shortName}:${version}"
 
-  curl -fsSL ${JENKINS_PLUGINS_URL}/${name}/${version}/${name}.hpi -o ${JENKINS_HOME}/plugins/${name}.jpi
+  curl -fsSL ${JENKINS_PLUGINS_URL}/${shortName}/${version}/${shortName}.hpi -o ${JENKINS_HOME}/plugins/${shortName}.jpi
 
-  unzip -qo ${JENKINS_HOME}/plugins/${name}.jpi
+  unzip -qo ${JENKINS_HOME}/plugins/${shortName}.jpi
 done
