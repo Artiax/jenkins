0c702329246591cf31333eaadc147059617bba5e
Deividas Sutkus <deividas.sutkus@qa.com>
2019-04-23 15:53:09 +0100

    Retrieve and parse RBAC permissions from consul kvs.

--- mappedFiles/bin/entrypoint.sh
+++ mappedFiles/bin/entrypoint.sh
@@ -3,6 +3,11 @@
 # Move baked in jobs to the persistent volume
 mv -f /tmp/jobs/* ${JENKINS_HOME}/jobs
 
+# Start consul-template daemon
+consul-template -once \
+  -consul-addr "${CONSUL_ADDR}" \
+  -template "/etc/consul-template/config.ctmpl:${JENKINS_HOME}/config.xml"
+
 # Start the main jenkins java process
 exec java -jar ${JENKINS_HOME}/jenkins.war \
   -Djenkins.install.runSetupWizard=false \
