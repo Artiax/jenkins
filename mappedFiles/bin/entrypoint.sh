6092f83496456845835f0d1cf006512a3f089ceb
Deividas Sutkus <deividas.sutkus@qa.com>
2019-04-23 17:12:43 +0100

    Consume configMap as mounted files - reduces the overhead of having to specify each environment variable on deployment descriptor.

--- mappedFiles/bin/entrypoint.sh
+++ mappedFiles/bin/entrypoint.sh
@@ -3,6 +3,11 @@
 # Move baked in jobs to the persistent volume
 mv -f /tmp/jobs/* ${JENKINS_HOME}/jobs
 
+# Convert config files (from the configMap) into environment variables
+for config in `ls /etc/config`; do
+  eval export $config=$(cat /etc/config/$config);
+done
+
 # Start consul-template daemon
 consul-template -once \
   -consul-addr "${CONSUL_ADDR}" \
